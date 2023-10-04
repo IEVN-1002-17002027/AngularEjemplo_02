f6c9d749e6f960549d72245277a97d421b778e95
Axel Rivas <17002027@alumnos.utleon.edu.mx>
2023-10-04 00:32:27 -0600

    Resubido AngularEjemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { Suma2Component } from './formularios/suma2/suma2.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
 import { MenuComponent } from './menu/menu/menu.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MaterialModule } from './material/material/material.module';
 
 @NgModule({
   declarations: [
@@ -29,11 +26,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatToolbarModule,
     MatSlideToggleModule,
-    MatIconModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
